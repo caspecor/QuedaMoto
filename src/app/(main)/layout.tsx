349ad416108f09c105d98aa1f139855b9f962af0
caspecor <ancorojeda@gmail.com>
2026-04-19 13:24:26 +0100

    fix: pass user session prop to Navbar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default async function MainLayout({ children }: { children: ReactNode }) 
 
   return (
     <div className="flex min-h-screen flex-col bg-background pb-16 md:pb-0">
-      <Navbar />
+      <Navbar user={user} />
       <main className="flex-1">
         {children}
       </main>
